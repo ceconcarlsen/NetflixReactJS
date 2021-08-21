7bd7d0699f2f7bf9b77ffc0dc1030a43276e1a54
ceconcarlsen <ceconcarlsen@gmail.com>
2021-08-21 00:18:15 -0300

    half done ...

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, {useEffect, useState} from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 //Dados
-import Tmdb from './Tmdb';
+import Tmdb from "./Tmdb";
 
 //Components
-import MovieRow from './Components/MovieRow/MovieRow';
-import FeaturedMovie from './Components/FeaturedMovie/FeaturedMovie';
+import MovieRow from "./Components/MovieRow/MovieRow";
+import FeaturedMovie from "./Components/FeaturedMovie/FeaturedMovie";
 
 function App() {
-
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState(null);
 
-  
-  useEffect(()=>{ //Quando a aplicação for carregada, executa a função
-    const loadAll = async () =>{
+  useEffect(() => {
+    //Quando a aplicação for carregada, executa a função
+    const loadAll = async () => {
       //Pegando a lista dos filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list); //Novo state da minha aplicação
 
       //Pegando o fimle em destaque
-      let originals = list.filter(i=>i.slug === "originals");
-      let randomChosen =  Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
-      console.log(chosen); 
-    }
-    
-    loadAll();
 
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeatureData(chosenInfo); //Info enviada ao componente FeaturedMovie
+    };
+
+    loadAll();
   }, []);
 
   return (
     <div className="page">
-      {featureData && 
-        <FeaturedMovie item={featureData }/>
-      }
-       <section className="lists">
-        {movieList.map((item, key)=>(
-            <MovieRow key={key} title={item.title} items={item.items}/>
+      {featureData && <FeaturedMovie item={featureData} />}
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
-       </section> 
+      </section>
     </div>
   );
 }
